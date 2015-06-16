e12db3320f3f23d368ba80cfc25ccc8a6476ee62
Frank Rem <frank@tallcomponents.com>
2015-06-16 16:58:11 +0200

    Display selected TextShape, ImageShape and FreeHandShape.

--- ShapesBrowser/MainWindow.xaml.cs
+++ ShapesBrowser/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace TallComponents.Samples.ShapesBrowser
         pdf.Document document;
         FileStream file;
         string path;
+        Canvas overlay;
 
         public MainWindow()
         {
@@ -42,7 +43,7 @@ namespace TallComponents.Samples.ShapesBrowser
             {
                 close();
                 open(dialog.FileName);
-            }            
+            }
         }
 
         void open(string path)
@@ -85,8 +86,12 @@ namespace TallComponents.Samples.ShapesBrowser
 
         void initializeShapesTree(pdf.Page page)
         {
+            ShapeCollectionConverter.Reset();
+
             pdf.Shapes.ShapeCollection shapes = page.CreateShapes();
-            (shapesTree.Items[0] as TreeViewItem).ItemsSource = shapes;
+            pdf.Shapes.ShapeCollection root = new pdf.Shapes.ShapeCollection();
+            root.Add(shapes);
+            (shapesTree.Items[0] as TreeViewItem).ItemsSource = root;
             (shapesTree.Items[0] as TreeViewItem).IsExpanded = true;
         }
 
@@ -100,7 +105,121 @@ namespace TallComponents.Samples.ShapesBrowser
                 pdf.Rasterizer.Configuration.RenderSettings renderSettings = new pdf.Rasterizer.Configuration.RenderSettings();
                 pdf.Rasterizer.Diagnostics.Summary summary = new pdf.Rasterizer.Diagnostics.Summary();
                 pdf.Rasterizer.ConvertToWpfOptions convertOptions = new pdf.Rasterizer.ConvertToWpfOptions();
-                documentViewer.Document = document.ConvertToWpf(renderSettings, convertOptions, index, index, summary);
+                FixedDocument fixedDocument = document.ConvertToWpf(renderSettings, convertOptions, index, index, summary);
+
+                documentViewer.Document = fixedDocument;
+
+                FixedPage fixedPage = fixedDocument.Pages[0].Child;
+
+                // add a canvas and change its coordinate system to that of PDF
+                overlay = new Canvas();
+                overlay.Width = fixedPage.Width;
+                overlay.Height = fixedPage.Height;
+                TransformGroup group = new TransformGroup();
+                group.Children.Add(new ScaleTransform(fixedPage.Width / page.Width, -fixedPage.Height / page.Height));
+                group.Children.Add(new TranslateTransform(0, fixedPage.Height));
+                overlay.RenderTransform = group;
+                fixedPage.Children.Add(overlay);
+
+                // now draw on the canvas in PDF coordinate space
+                System.Windows.Shapes.Line line = new System.Windows.Shapes.Line();
+                line.Stroke = new SolidColorBrush(Colors.Red);
+                line.StrokeThickness = 3;
+                line.X1 = line.Y1 = 0;
+                line.X2 = page.Width;
+                line.Y2 = page.Height;
+                overlay.Children.Add(line);
+            }
+        }
+
+        private void shapesTree_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            overlay.Children.Clear();
+
+            pdf.Shapes.ContentShape shape = shapesTree.SelectedItem as pdf.Shapes.ContentShape;
+            if (null != shape)
+            {
+                TransformGroup group = new TransformGroup(); 
+                Matrix matrix = shape.Transform.CreateWpfMatrix();
+                group.Children.Add(new MatrixTransform(matrix));
+
+                pdf.Shapes.ShapeCollection parent = ShapeCollectionConverter.GetParent(shape);
+                while (null != parent)
+                {
+                    matrix = parent.Transform.CreateWpfMatrix();
+                    group.Children.Add(new MatrixTransform(matrix));
+
+                    parent = ShapeCollectionConverter.GetParent(parent);
+                }
+                
+                System.Windows.Shapes.Rectangle rectangle = new System.Windows.Shapes.Rectangle();
+                rectangle.RenderTransform = group;
+                
+                rectangle.Fill = new SolidColorBrush(Color.FromArgb(64, 255, 0, 0));
+
+                if (shape is pdf.Shapes.TextShape)
+                {
+                    pdf.Shapes.TextShape text = shape as pdf.Shapes.TextShape;
+                    rectangle.Width = text.MeasuredWidth;
+                    rectangle.Height = text.MeasuredHeight;
+
+                    overlay.Children.Add(rectangle);
+                }
+                else if (shape is pdf.Shapes.ImageShape)
+                {
+                    pdf.Shapes.ImageShape image = shape as pdf.Shapes.ImageShape;
+                    rectangle.Width = image.Width;
+                    rectangle.Height = image.Height;
+
+                    overlay.Children.Add(rectangle);
+                }
+                else if (shape is pdf.Shapes.FreeHandShape)
+                {
+                    pdf.Shapes.FreeHandShape freehand = shape as pdf.Shapes.FreeHandShape;
+
+                    System.Windows.Shapes.Path path = new System.Windows.Shapes.Path();
+                    PathGeometry geometry = new PathGeometry();
+                    path.Data = geometry;
+
+                    foreach (pdf.Shapes.FreeHandPath freehandPath in freehand.Paths)
+                    {
+                        PathFigure figure = new PathFigure();
+                        figure.IsClosed = freehandPath.Closed;
+
+                        foreach (pdf.Shapes.FreeHandSegment segment in freehandPath.Segments)
+                        {
+                            if (segment is pdf.Shapes.FreeHandStartSegment)
+                            {
+                                pdf.Shapes.FreeHandStartSegment start = segment as pdf.Shapes.FreeHandStartSegment;
+                                figure.StartPoint = new Point(start.X, start.Y);
+                            }
+                            else if (segment is pdf.Shapes.FreeHandLineSegment)
+                            {
+                                pdf.Shapes.FreeHandLineSegment line = segment as pdf.Shapes.FreeHandLineSegment;
+                                figure.Segments.Add(new LineSegment(new Point(line.X1, line.Y1), true));
+                            }
+                            else if (segment is pdf.Shapes.FreeHandBezierSegment)
+                            {
+                                pdf.Shapes.FreeHandBezierSegment bezier = segment as pdf.Shapes.FreeHandBezierSegment;
+                                figure.Segments.Add(new BezierSegment(
+                                    new Point(bezier.X1, bezier.Y1),
+                                    new Point(bezier.X2, bezier.Y2),
+                                    new Point(bezier.X3, bezier.Y3),
+                                    true));
+                            }
+                        }
+
+                        geometry.Figures.Add(figure);
+                    }
+
+                    path.Fill = new SolidColorBrush(Colors.Red);
+                    path.Stroke = new SolidColorBrush(Colors.Green);
+                    path.StrokeThickness = 1;
+                    path.RenderTransform = group;
+
+                    overlay.Children.Add(path);
+
+                }
             }
         }
     }
