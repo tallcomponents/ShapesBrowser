ae945ad951c6ffa9b05667e67ac32a8eeac4f17d
Frank Rem <frank@tallcomponents.com>
2015-06-16 10:49:01 +0200

    First import

---
+++ ShapesBrowser/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+using Microsoft.Win32;
+
+using pdf = TallComponents.PDF;
+
+namespace WpfApplication1
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        pdf.Document document;
+        FileStream file;
+        string path;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "PDF files (*.pdf)|*.pdf";
+            if (dialog.ShowDialog() == true)
+            {
+                close();
+                open(dialog.FileName);
+            }            
+        }
+
+        void open(string path)
+        {
+            this.path = path;
+            file = new FileStream(path, FileMode.Open, FileAccess.Read);
+            document = new pdf.Document(file);
+            initializePagesList();
+        }
+
+        void close()
+        {
+            if (null != file)
+            {
+                file.Dispose();
+                file = null;
+                document = null;
+                path = null;
+            }
+        }
+
+        void initializePagesList()
+        {
+            if (null != document)
+            {
+                pagesList.ItemsSource = document.Pages;
+                pagesList.SelectedIndex = 0;
+            }
+        }
+
+        private void pagesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            pdf.Page page = pagesList.SelectedItem as pdf.Page;
+            if (null != page)
+            {
+                initializeShapesTree(page);
+                drawPage(page.Index);
+            }
+        }
+
+        void initializeShapesTree(pdf.Page page)
+        {
+            pdf.Shapes.ShapeCollection shapes = page.CreateShapes();
+            (shapesTree.Items[0] as TreeViewItem).ItemsSource = shapes;
+            (shapesTree.Items[0] as TreeViewItem).IsExpanded = true;
+        }
+
+        void drawPage(int index)
+        {
+            using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                pdf.Rasterizer.Document document = new pdf.Rasterizer.Document(file);
+                pdf.Rasterizer.Page page = document.Pages[index];
+
+                pdf.Rasterizer.Configuration.RenderSettings renderSettings = new pdf.Rasterizer.Configuration.RenderSettings();
+                pdf.Rasterizer.Diagnostics.Summary summary = new pdf.Rasterizer.Diagnostics.Summary();
+                pdf.Rasterizer.ConvertToWpfOptions convertOptions = new pdf.Rasterizer.ConvertToWpfOptions();
+                documentViewer.Document = document.ConvertToWpf(renderSettings, convertOptions, index, index, summary);
+            }
+        }
+    }
+}
---
+++ ShapesBrowser/ShapeCollectionConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+using TallComponents.PDF.Shapes;
+
+namespace WpfApplication1
+{
+    public class ChildShapes
+    {
+        public ChildShapes(ShapeCollection shapes)
+        {
+            Shapes = new List<Shape>(shapes);
+        }
+
+        public List<Shape> Shapes { get; set;  }
+
+        public string Display { get { return string.Format("{0} child shapes", Shapes.Count); } }
+    }
+
+    public class SimpleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new List<object>(values);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ShapeCollectionConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            List<object> items = new List<object>();
+
+            foreach (object v in values)
+            {
+                if (v is ShapeCollection)
+                {
+                    items.Add(new ChildShapes(v as ShapeCollection));
+                }
+                else
+                {
+                    items.Add(v);
+                }
+            }
+
+            return items;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
