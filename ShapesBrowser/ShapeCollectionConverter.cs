8f9bc43d7dce3ad667e269abc86f654f478e0ffd
Frank Rem <frank@tallcomponents.com>
2015-06-16 16:57:12 +0200

    Mechanism for finding the parent ShapeCollection of a Shape.
    The mchanism uses the Shape.ID property to encode the parent-child relationship.

--- ShapesBrowser/ShapeCollectionConverter.cs
+++ ShapesBrowser/ShapeCollectionConverter.cs
@@ -34,8 +34,32 @@ namespace TallComponents.Samples.ShapesBrowser
         }
     }
 
+
     public class ShapeCollectionConverter : IMultiValueConverter
     {
+        static List<ShapeCollection> shapeCollections = new List<ShapeCollection>();
+
+        internal static void Reset()
+        {
+            shapeCollections.Clear();
+        }
+
+        internal static ShapeCollection GetParent(Shape shape)
+        {
+            int index = 0;
+            if (shape is ShapeCollection)
+            {
+                if (shape.ID == null || shape.ID[0] == ':') return null;
+                index = int.Parse(shape.ID.Substring(0, shape.ID.IndexOf(':')));
+            }
+            else
+            {
+                index = int.Parse(shape.ID);
+            }
+
+            return shapeCollections[index];
+        }
+
         public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             List<object> items = new List<object>();
@@ -44,6 +68,18 @@ namespace TallComponents.Samples.ShapesBrowser
             {
                 if (v is ShapeCollection)
                 {
+                    ShapeCollection shapes = v as ShapeCollection;
+                    shapeCollections.Add(shapes);
+                    int id = shapeCollections.Count - 1;
+                    // the ID of a ShapeCollection is formatted as "parent-id:self-id"
+                    // the OD of all other shapes is formatted as "parent-id"
+                    shapes.ID = string.Format("{0}:{1}", shapes.ID, id);
+
+                    foreach (Shape shape in shapes)
+                    {
+                        shape.ID = id.ToString();
+                    }
+
                     items.Add(new ChildShapes(v as ShapeCollection));
                 }
                 else
