97b5342d974564bc15a1968574b15f4f09ba22c9
Martin Strapko <strapko@digitaldocuments.org>
2020-03-24 09:55:00 +0100

    Removed unused mouse tracking decorator.

--- ShapesBrowser/ViewModels/MainWindowViewModel.cs
+++ ShapesBrowser/ViewModels/MainWindowViewModel.cs
@@ -30,7 +30,6 @@ namespace TallComponents.Samples.ShapesBrowser
         private double _scaleY;
         private double _translateOrientationX;
         private double _translateOrientationY;
-        private Point _mousePosition;
 
         public MainWindowViewModel(IDialogBoxService dialogBoxService)
         {
